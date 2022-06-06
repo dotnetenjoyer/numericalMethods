1424c1d91ac5a8889051918303e2735bcda45c7f
a.shchagolchin <lesha.shhagolchin@mail.ru>
2022-06-07 01:55:25 +0700

    feat(core, tests): add successive approximations method

--- NumericalMethods.Core/Methods/SuccessiveApproximations.cs
+++ NumericalMethods.Core/Methods/SuccessiveApproximations.cs
@@ -1,7 +1,44 @@
-﻿namespace NumericalMethods.Core.Methods
+﻿using System;
+using NumericalMethods.Tests.Extensions;
+
+namespace NumericalMethods.Core.Methods
 {
 	public class SuccessiveApproximations
 	{
+		private readonly Func<double, double> _function;
+		private readonly Func<double, double> _iterationFunction;
+		private readonly double _accuracy;
+		private readonly double _firstApproach;
+
+		private int _iterationCounter;
 		
-	}
+		public SuccessiveApproximations(Func<double, double> function, Func<double, double> iterationFunction, double accuracy, double firstApproach)
+		{
+			_function = function;
+			_iterationFunction = iterationFunction;
+			_accuracy = accuracy;
+			_firstApproach = firstApproach;
+		}
+
+		public int IterationCounter => _iterationCounter;
+
+		public double Process()
+		{
+			double currentApproach = _firstApproach;
+
+			do
+			{
+				currentApproach = _iterationFunction(currentApproach);
+				_iterationCounter++;
+			} while (!IsAcceptableRootOfEquation(currentApproach));
+
+			return currentApproach;
+		}
+		
+		private bool IsAcceptableRootOfEquation(double argument)
+		{
+			var functionValue = _function(argument);
+			return functionValue.EqualWithTolerance(0, _accuracy);
+		}
+	} 
 }
---
+++ NumericalMethods.Tests/SuccessiveApproximationsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NumericalMethods.Core.Methods;
+using NumericalMethods.Tests.Extensions;
+using NUnit.Framework;
+
+namespace NumericalMethods.Tests
+{
+	public class SuccessiveApproximationsTests
+	{
+		private readonly Func<double, double> _function = x => Math.Sin(x) + 2 * Math.Pow(x, 2) - 5;
+		private readonly Func<double, double> _iterationFunction = x => Math.Sqrt((5 - Math.Sin(x)) / 2);
+		private readonly double _accuracy = 0.0001;
+		
+		[Test]
+		public void Process_ShouldPass()
+		{
+			//arrange 
+			var firstApproach = -2;
+			var successiveApproximations = new SuccessiveApproximations(_function, _iterationFunction, _accuracy, firstApproach);
+
+			//act 
+			var root = successiveApproximations.Process();
+			TestContext.Out.WriteLine($"{successiveApproximations.IterationCounter} iterated");
+			
+			//arrange
+			Assert.IsTrue(root.EqualWithTolerance(1.4163, _accuracy));
+		}
+	}
+}
