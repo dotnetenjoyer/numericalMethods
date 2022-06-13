e766bdd54fe73c086c53bb13b9a28f2932e84a5d
a.shchagolchin <lesha.shhagolchin@mail.ru>
2022-06-13 23:54:52 +0700

    feat(core): add lagrangian polynomials

---
+++ NumericalMethods.Core/Methods/LagrangianPolynomials.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace NumericalMethods.Core.Methods
+{
+	public class LagrangianPolynomials
+	{
+		private readonly Point[] _points;
+
+		public LagrangianPolynomials(Point[] points)
+		{
+			_points = points;
+		}
+
+		public Func<double, double> Process()
+		{
+			return x => _points.Sum(point => point.Y * FindBasePolynomialValue(point.X, x));
+		}
+
+		public double FindBasePolynomialValue(double xi, double x)
+		{
+			double value = 1;
+			
+			foreach (var point in _points.Where(x => x.X != xi))
+			{
+				value *= (x - point.X) / (xi - point.X);
+			}
+			
+			return value;
+		}
+	}
+}
---
+++ NumericalMethods.Tests/LagrangianPolynomialsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NumericalMethods.Core;
+using NumericalMethods.Core.Methods;
+using NUnit.Framework;
+
+namespace NumericalMethods.Tests
+{
+	public class LagrangianPolynomialsTests
+	{
+		private Func<double, double> _function => x => Math.Pow(Math.Log10(x), 10 / 7);
+
+		[Test]
+		public void Process_ShouldPass()
+		{	
+			//arrange
+			Point[] points =
+			{
+				new (6, _function(6)),
+				new (9, _function(9)),
+				new (12, _function(12))
+			};
+
+			var lagrangianPolynomials = new LagrangianPolynomials(points);
+
+			//act
+			var interpolationPolynomial = lagrangianPolynomials.Process();
+			var error = _function(10.5) - interpolationPolynomial(10.5);
+			TestContext.Out.WriteLine($"Error: {error}");
+			
+			//assert
+			Assert.IsTrue(Math.Abs(error) < 0.01);
+		}
+	}
+}
