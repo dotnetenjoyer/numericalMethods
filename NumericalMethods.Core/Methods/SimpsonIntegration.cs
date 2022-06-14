7c2b7c89602230cb5bc68733e3588febd7b8250d
a.shchagolchin <lesha.shhagolchin@mail.ru>
2022-06-14 09:29:39 +0700

    feat(core, tests): add simpson integration method

---
+++ NumericalMethods.Core/Methods/SimpsonIntegration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NumericalMethods.Core.Methods
+{
+	public class SimpsonIntegration
+	{
+		private readonly Interval _interval;
+		private readonly Func<double, double> _function;
+		private readonly int _numberOfStep;
+		private readonly double _step;
+
+		public SimpsonIntegration(Func<double, double> function, Interval interval, int numberOfStep)
+		{
+			_function = function;
+			_interval = interval;
+			_numberOfStep = numberOfStep;
+			_step = _interval.Length / numberOfStep;
+		}
+
+		public double Process()
+		{
+			double integral = 0;
+
+			for (int i = 0; i < _numberOfStep; i += 2)
+			{
+				double y0 = _function(_interval.A + _step * i);
+				double y1 = _function(_interval.A + _step * (i + 1));
+				double y2 = _function(_interval.A + _step * (i + 2));
+
+				integral += _step / 3 * (y0 + 4 * y1 + y2);
+			}
+			
+			return integral;
+		}
+	}
+}
---
+++ NumericalMethods.Tests/SimpsonIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NumericalMethods.Core;
+using NumericalMethods.Core.Methods;
+using NUnit.Framework;
+
+namespace NumericalMethods.Tests
+{
+	public class SimpsonIntegrationTests
+	{
+		[Test]
+		public void Process_ShouldPass()
+		{
+			//arrange
+			var interval = new Interval(2, 4);
+			Func<double, double> function = x => Math.Pow(Math.Log(x), 10 / 7);
+
+			var simpsonIntegration = new SimpsonIntegration(function, interval, 1000);
+			
+			//act
+			var integral = simpsonIntegration.Process();
+			
+			//assert
+			Assert.IsTrue(integral > 2.1);
+		}
+	}
+}
